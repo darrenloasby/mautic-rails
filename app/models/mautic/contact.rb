0ad7542365fe0c87018fbea1d2cd70b93b6bc3d5
Darren Loasby <darren.loasby@gmail.com>
2018-12-15 21:20:05 +1100

    Update contact.rb

--- app/models/mautic/contact.rb
+++ app/models/mautic/contact.rb
@@ -12,9 +12,8 @@ module Mautic
     end
 
     def add_do_not_contact(channel='email')
-      puts(self)
-      #save if id.blank?
-      connection.refresh!.request(:post, "api/contacts/#{id}/dnc/#{channel}/add")
+      save if id.blank?
+      connection.request(:post, "api/contacts/#{id}/dnc/#{channel}/add")
     end
     
     def assign_attributes(source = nil)
